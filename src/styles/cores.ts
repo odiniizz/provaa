ee600533671452aade3eaf9193db5d9c3b39c6a0
odiniizz <jcesarddiniz05@gmail.com>
2024-11-14 08:29:56 -0300

    ata6

--- src/styles/cores.ts
+++ src/styles/cores.ts
@@ -1,8 +1,6 @@
 export const cores = {
     primary: '#A020F0',
-    secondary: '#BA661D',
-    third: '#D9D9D9',
-    fourth: '#5C415D',
+    secondary: '#FFD700',
     black: '#000',
     white: '#FFF'
 }
